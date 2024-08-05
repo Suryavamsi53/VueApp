b276413dd79c6a17147e5dc2b9bd69cca7c5f3da
Suryavamsi <63606535+Suryavamsi53@users.noreply.github.com>
2024-08-05 11:39:58 +0530

    Update axiosLookup.js

--- src/axiosLookup.js
+++ src/axiosLookup.js
@@ -41,3 +41,44 @@ methods: {
       });
   }
 }
+
+
+
+
+<!--    
+import axios from 'axios';
+
+// Create an axios instance
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:5000', // Replace with your WCF service base URL
+  timeout: 10000, // Request timeout
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+  config => {
+    // Do something before request is sent, like adding authorization headers
+    // config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  error => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+  response => response,
+  error => {
+    // Handle response errors
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
+
+-->
